5fc53a35e04335db04a60a951411818d0daa8dde
Vladimir Morar <morvladimir1981@gmail.com>
2023-02-02 22:14:16 +0300

    add example 01

---
+++ example01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.Write("Введите целое число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите целое число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+while(numberB > 0)
+{
+    result *= numberA;
+    numberB -= 1; 
+}
+
+System.Console.WriteLine(result);
